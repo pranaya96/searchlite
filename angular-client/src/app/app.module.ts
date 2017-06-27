c469209489852af5574ce6b5a3af467069aaae91
Amckoy <ashley.mckoy@bison.howard.edu>
2017-06-26 23:18:48 -0700

    Added toolbar

--- angular-client/src/app/app.module.ts
+++ angular-client/src/app/app.module.ts
@@ -2,10 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MdButtonModule, MdInputModule, MdListModule } from '@angular/material';
+import { MdButtonModule, MdInputModule, MdListModule, MdToolbarModule, MdIconModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SearchService } from './search.service';
-
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -19,6 +18,9 @@ import { AppComponent } from './app.component';
     MdInputModule,
     HttpModule,
     MdListModule,
+    MdToolbarModule,
+    MdButtonModule,
+    MdIconModule,
   ],
   providers: [SearchService],
   bootstrap: [AppComponent]
