ff898fa32c87df8a4963d094a67371a2cefd1edd
Kyle Frederick <pranaya.adhikari@bison.howard.edu>
2017-06-28 16:01:10 -0700

    Help

--- angular-client/src/app/home/home.component.ts
+++ angular-client/src/app/home/home.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import {SearchService} from '../search.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+  term: string;
+  results: string[];
+  title = 'Got qWest?';
 
-  constructor() { }
+  constructor(private searchService: SearchService) {}
 
-  ngOnInit() {
+  search() {
+    if (this.term === 'dog')
+    {
+      this.results = ['file1.pdf'];
+    }
+    if (this.term == 'Nepal')
+    {
+      this.results = ['Nepal is a great place'];
+    }
+    if (this.term == 'America')
+    {
+      this.results = ['Some link to America'];
+    }
+    
+
   }
 
 }
